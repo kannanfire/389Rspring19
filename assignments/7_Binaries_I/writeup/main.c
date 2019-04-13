c68f6ff19ec4dd0934cb391f8182573d0a443793
kannanfire <adhithya.kannan@gmail.com>
2019-04-13 13:17:38 -0400

    updating to vm

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,6 +1,6 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: Adhithya Kannan
+ * Section: 0101
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
